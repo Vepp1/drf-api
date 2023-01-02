a6bdcbf165e6462c0313618150f0ce0d6e79f65d
Vepp1 <vinipontarolla@gmail.com>
2023-01-02 13:43:44 +0000

    Fix exposed secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mhq#@w2m%xgh6&g&e3+yq*rs$6$@4nakyh$g-p-372$+zchsw9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
