c6e368e3b5493ee992e20957051bc6186ac0c49a
Vepp <vinipontarolla@gmail.com>
2023-01-06 17:09:08 +0100

    Add CommentList and CommentDetail generic views

--- comments/serializers.py
+++ comments/serializers.py
@@ -9,17 +9,16 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
-        request = self.context('request')
+        request = self.context['request']
         return request.user == obj.owner
 
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'is_owner', 'post'
-            'created_at', 'updated_at', 'content',
-            'profile_id', 'profile_image'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
         ]
 
 
 class CommentDetailSerializer(CommentSerializer):
-    post = serializers.ReadOnlyField(source-'post.id')
+    post = serializers.ReadOnlyField(source='post.id')
