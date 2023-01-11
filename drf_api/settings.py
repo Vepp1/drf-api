38b6ee20b137326441d8b8162ddae779fa0f6e72
Vepp <vinipontarolla@gmail.com>
2023-01-11 16:30:28 +0100

    Fix allowed host error

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', os.environ.get['ALLOWED_HOST'],]
+ALLOWED_HOSTS = ['localhost', os.environ.get('ALLOWED_HOST'),]
 
 
 # Application definition
