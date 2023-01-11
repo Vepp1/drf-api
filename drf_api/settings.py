a2f39f42eec8514b823717474dab1520b8199130
Vepp <vinipontarolla@gmail.com>
2023-01-11 12:32:00 +0100

    Add django-rest-auth

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -49,14 +49,15 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
     'profiles',
     'posts',
     'comments',
     'likes',
     'followers',
-    'rest_framework',
     'django_filters',
-
 ]
 
 MIDDLEWARE = [
